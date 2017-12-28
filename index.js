39516448a927ff62021773877b61cfa33b4db8f8
Matt States <mattstates@Matts-MacBook-Pro.local>
2017-12-28 13:20:43 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+app.use(express.static('client'))
+app.get('/', (req, res) => res.send('index.html'));
+
+app.listen(PORT, ()=> console.log(`I am listening on ${PORT}`))
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import d3 from 'd3';
+console.log('I am the client code and I should run.')
+console.log(d3)
