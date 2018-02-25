106624230f5ba152e14a18dcf71bc1f32d8df2a1
Matt States <matt@mattstates.com>
2018-02-24 23:30:21 -0800

    server debug

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,10 +3,12 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 module.exports = merge(common, {
-    //devtool: 'source-map',
+    devtool: 'source-map',
     plugins: [
         new UglifyJSPlugin({
-            sourceMap: true
+            test: /\.js($|\?)/i,
+            sourceMap: true,
+            parallel: 4
         }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
